5de0332a4107ef940efbd91afc5b340459472e16
slntopp <slnt_opp@pm.me>
2019-05-23 05:36:51 +0200

    Appending hooks

--- install.rb
+++ install.rb
@@ -18,6 +18,9 @@ sh.system `yum install -y npm make automake gcc gcc-c++ kernel-devel ruby-devel 
 puts "Installing bower and grunt"
 sh.system 'npm install -g bower grunt grunt-cli'
 
+puts "Setting hooks up"
+sh.system "cp -rf hooks /usr/lib/one/remotes/" 
+
 puts "Moving sunstone src files"
 sunstone = %w(
     models public routes views ione
@@ -48,3 +51,102 @@ sh.cd src_dir
 sh.system 'cp -f ./sunstone-views.yaml /etc/one/'
 sh.system 'cp -rf ./sunstone-views /etc/one/'
 # sh.system 'cp -f ./ione/ione.conf /etc/one/'
+
+puts 'Appending hooks to oned.conf'
+
+hooks = 
+'VM_HOOK = [
+    name      = "set_price",
+    on        = "CREATE",
+    command   = "set_price.rb",
+    arguments = "$ID"
+]
+
+VM_HOOK = [
+    name      = "pending",
+    on        = "CUSTOM",
+    state     = "PENDING",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "pending",
+    on        = "CUSTOM",
+    state     = "HOLD",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "active",
+    on        = "CUSTOM",
+    state     = "ACTIVE",
+    lcm_state = "BOOT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "active",
+    on        = "CUSTOM",
+    state     = "ACTIVE",
+    lcm_state = "RUNNING",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "inactive",
+    on        = "CUSTOM",
+    state     = "STOPPED",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "inactive",
+    on        = "CUSTOM",
+    state     = "SUSPENDED",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "inactive",
+    on        = "CUSTOM",
+    state     = "DONE",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "inactive",
+    on        = "CUSTOM",
+    state     = "POWEROFF",
+    lcm_state = "LCM_INIT",
+    command   = "record.rb",
+    arguments = "$ID" ]
+
+VM_HOOK = [
+    name      = "set_limits",
+    on        = "RUNNING",
+    command   = "vcenter/set_limits.rb",
+    arguments = "$ID $PREV_STATE $PREV_LCM_STATE" ]
+'
+
+hooks +=
+'USER_HOOK = [
+    name = "reserve_ar_on_create",
+    on = "CREATE",
+    command = "set_ar.rb",
+    arguments = "$ID" ]
+
+USER_HOOK = [
+    name = "release_ar_on_remove",
+    on = "REMOVE",
+    command = "remove_ar.rb",
+    arguments = "$TEMPLATE" ]
+'
+
+File.open('/etc/one/oned.conf', 'a') do conf
+    conf << hooks
+end
