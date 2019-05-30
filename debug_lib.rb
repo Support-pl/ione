1a878c5c196eab8d541e55f050004c30d7e6a66a
slntopp <slnt_opp@pm.me>
2019-05-30 03:10:56 +0200

    debug lib

--- debug_lib.rb
+++ debug_lib.rb
@@ -43,6 +43,56 @@ GROUP_ADMIN_DEFAULT_VIEW_XPATH = 'TEMPLATE/SUNSTONE/GROUP_ADMIN_DEFAULT_VIEW'
 TABLE_DEFAULT_PAGE_LENGTH_XPATH = 'TEMPLATE/SUNSTONE/TABLE_DEFAULT_PAGE_LENGTH'
 LANG_XPATH = 'TEMPLATE/SUNSTONE/LANG'
 
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'yaml'
+require 'securerandom'
+require 'tmpdir'
+require 'fileutils'
+require 'base64'
+require 'rexml/document'
+require 'uri'
+require 'open3'
+
+require 'CloudAuth'
+require 'SunstoneServer'
+require 'SunstoneViews'
+
+
+begin
+    $conf = YAML.load_file(CONFIGURATION_FILE)
+rescue Exception => e
+    STDERR.puts "Error parsing config file #{CONFIGURATION_FILE}: #{e.message}"
+    exit 1
+end
+
+if $conf[:one_xmlrpc_timeout]
+    ENV['ONE_XMLRPC_TIMEOUT'] = $conf[:one_xmlrpc_timeout].to_s unless ENV['ONE_XMLRPC_TIMEOUT']
+end
+
+$conf[:debug_level] ||= 3
+
+# Set Sunstone Session Timeout
+$conf[:session_expire_time] ||= 3600
+
+# Set the TMPDIR environment variable for uploaded images
+ENV['TMPDIR']=$conf[:tmpdir] if $conf[:tmpdir]
+
+include CloudLogger
+logger=enable_logging(SUNSTONE_LOG, $conf[:debug_level].to_i)
+
+begin
+    ENV["ONE_CIPHER_AUTH"] = SUNSTONE_AUTH
+    $cloud_auth = CloudAuth.new($conf, logger)
+rescue => e
+    logger.error {
+        "Error initializing authentication system" }
+    logger.error { e.message }
+    exit -1
+end
+
+DEBUG_LIB = true
 begin
     require 'ione/server/ione.rb'
 rescue => e
