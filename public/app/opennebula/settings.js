003a18c5a4b5b4be1c3c8168377df1011c253862
slnt_opp <nik.ivanovskij@gmail.com>
2019-06-04 12:21:43 +0200

    Settings

--- public/app/opennebula/settings.js
+++ public/app/opennebula/settings.js
@@ -31,8 +31,6 @@ define(function(require){
                 type: 'POST',
                 data: data,
                 success: function(req, res){
-                    console.log(1,req);
-                    console.log(2,res);
                     return callback ? callback(req, res) : null;
                 },
                 error: function(req, res){ Notifier.notifyError(req) }
@@ -47,8 +45,6 @@ define(function(require){
                 type: 'GET',
                 data: data,
                 success: function(req, res){
-                    console.log(1,req);
-                    console.log(2,res);
                     return callback ? callback(req, res) : null;
                 },
                 error: function(req, res){ Notifier.notifyError(req) }
