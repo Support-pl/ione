efce36d5a5eacaba063afe5a58249a1c85649cc6
slntopp <slnt_opp@protonmail.com>
2019-07-17 16:53:39 +0200

    Autofill fix

--- public/app/tabs/settings-tab/panels/cloud.js
+++ public/app/tabs/settings-tab/panels/cloud.js
@@ -1,5 +1,5 @@
 /* —------------------------------------------------------------------------ */
-/* Copyright 2018-2019, IONe Clouf Project                                    */
+/* Copyright 2002-2017, OpenNebula Project, OpenNebula Systems                */
 /*                                                                            */
 /* Licensed under the Apache License, Version 2.0 (the "License"); you may    */
 /* not use this file except in compliance with the License. You may obtain    */
