fbdfd0b07e79c764d2f8c58c1390dcae542f7af2
Mikita Iwanowski <info@slnt-opp.xyz>
2020-09-08 13:26:03 +0200

    templates list handler endpoint

---
+++ ione/server/lib/template_info/main.rb
@@ -0,0 +1,21 @@
+class IONe
+    def get_templates_list group_id = nil
+        LOG_STAT()
+        id = id_gen()
+        LOG_CALL(id, true, __method__)
+        defer { LOG_CALL(id, false, 'get_templates_list') }
+        tp_pool = TemplatePool.new($client)
+        tp_pool.info_all!
+        tp_pool.inject([]) do | res, tp |
+            res << {
+                id: tp.id,
+                name: tp.name,
+                description: tp.to_hash['VMTEMPLATE']['TEMPLATE']['DESCRIPTION']
+                logo: tp.to_hash['VMTEMPLATE']['TEMPLATE']['LOGO'],
+                uid: tp.to_hash['VMTEMPLATE']['UID'],
+                gid: tp.to_hash['VMTEMPLATE']['GID'],
+            } if group_id.nil? or group_id.to_i == tp.to_hash['VMTEMPLATE']['GID'].to_i
+            res
+        end
+    end
+end
