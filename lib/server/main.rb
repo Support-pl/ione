95999c8c9117c6aea184208ad8793c1000cd8a38
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-14 01:00:44 +0100

    deprecated

--- lib/server/main.rb
+++ lib/server/main.rb
@@ -4,15 +4,6 @@
 
 puts 'Extending Handler class by server-info getters'
 class IONe
-    # @api private    
-    # Returns thread locks stats
-    def locks_stat(key = nil)
-        LOG_STAT()
-        id = id_gen()
-        LOG_CALL(id, true, __method__)
-        defer { LOG_CALL(id, false, 'locks_stat') }
-        $thread_locks
-    end
     # Returns current running IONe Cloud Server version
     # @return [String]
     def version
@@ -34,5 +25,11 @@ class IONe
     # Returns active processes list
     def proc
         $PROC
+    end
+
+    # Returns whole IONe settings table if user is Admin
+    # @return [Hash]
+    def get_all_settings
+        return @db[:settings].as_hash(:name, :body) if onblock(:u, -1, @client).is_admin
     end
 end
