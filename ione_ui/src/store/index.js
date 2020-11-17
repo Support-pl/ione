55328105c5d59416fd55d1c02fd36b0975ef897f
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-17 21:04:59 +0100

    store base

--- ione_ui/src/store/index.js
+++ ione_ui/src/store/index.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {
+      loggedIn: false,
+    },
   },
   mutations: {
+    login(state, user) {
+      state.loggedIn = user;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
