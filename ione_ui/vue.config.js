4358ffb9bb90766f89d161849808d3c804124d3e
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-17 20:01:43 +0100

    webpack

---
+++ ione_ui/vue.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@", path.resolve(__dirname, "src"));
+  },
+  devServer: {
+    host: "0.0.0.0",
+    hot: true,
+    disableHostCheck: true,
+  },
+};
