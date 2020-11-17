6787500717e9fa51604b5db39d1dd553cf79d00f
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-17 21:04:51 +0100

    axios set up

--- ione_ui/src/main.js
+++ ione_ui/src/main.js
@@ -2,6 +2,11 @@ import Vue from "vue";
 import App from "@/App.vue";
 import store from "@/store";
 import router from "@/router";
+import axios from "axios";
+
+Vue.prototype.$axios = axios.create({
+  baseURL: "http://185.66.69.108:8009",
+});
 
 Vue.config.productionTip = false;
 
