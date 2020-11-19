92d649440b6e978ecb4b3fad54416f7636867e64
Mikita Iwanowski <info@slnt-opp.xyz>
2020-11-19 19:02:54 +0100

    uncomplexifying

--- ione_ui/src/components/types/object.vue
+++ ione_ui/src/components/types/object.vue
@@ -1,46 +1,19 @@
 <template>
-  <div class="openable">
-    <template v-if="!opened">
-      <a-icon type="caret-down" @click="open()" />
-      {{ value.body }}
-    </template>
-    <template v-else>
-      <a-icon type="caret-up" @click="close()" />
-      <item
-        v-for="(element, key) in parse(value.body)"
-        :key="key"
-        :value="{ [key]: element }"
-      />
-    </template>
+  <div>
+    <div style="padding-left: 15px" v-for="[key, value] of pairs()" :key="key">
+      {{ key }}: {{ value }}
+    </div>
   </div>
 </template>
 
 <script>
-import item from "./objectItem.vue";
 export default {
   name: "settingsOBJECT",
-  data() {
-    return {
-      opened: false,
-    };
-  },
-  components: {
-    item,
-  },
   props: ["value"],
   methods: {
-    parse(str) {
-      return JSON.parse(str);
-    },
-    open() {
-      this.opened = true;
-    },
-    close() {
-      this.opened = false;
+    pairs() {
+      return Object.entries(JSON.parse(this.value.body ?? ""));
     },
   },
 };
 </script>
-
-<style>
-</style>
