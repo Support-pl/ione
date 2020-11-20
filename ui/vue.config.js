6c0d62c57fb2c6e5226106c694a53cb06b8ad32b
Mik <info@slnt-opp.xyz>
2020-11-20 13:55:23 +0100

    build destination and conf

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -3,9 +3,13 @@ module.exports = {
   chainWebpack: (config) => {
     config.resolve.alias.set("@", path.resolve(__dirname, "src"));
   },
+
   devServer: {
     host: "0.0.0.0",
     hot: true,
     disableHostCheck: true,
   },
+
+  runtimeCompiler: true,
+  productionSourceMap: false
 };
