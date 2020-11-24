172d9f757890de49b839220c910bb38f057b1887
Mik <info@slnt-opp.xyz>
2020-11-24 16:59:10 +0100

    moved method_missing

--- ione_server.rb
+++ ione_server.rb
@@ -179,6 +179,16 @@ class IONe
         @db = db
         @version = VERSION
     end
+
+    # Will call object method if smth like vm_poweroff(1) called
+    def method_missing m, *args, &block
+        obj, method = m.to_s.split('_')
+        if ONeHelper::ON_INSTANCES.keys.include? obj.to_sym then
+            onblock(obj.to_sym, args[0]).send(method, self)
+        else
+            super
+        end
+    end
 end
 
 ione_drivers = %w( AnsibleDriver AzureDriver ShowbackDriver IONeCustomActions)
