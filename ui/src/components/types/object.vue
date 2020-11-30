3777f6427b2c30591c5149fdb01de3543a682335
dowellkin <dowellkin@gmail.com>
2020-11-30 17:07:59 +0300

    decompose object key and value into two different ojects

--- ui/src/components/types/object.vue
+++ ui/src/components/types/object.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <div style="padding-left: 15px" v-for="[key, value] of pairs()" :key="key">
-      {{ key }}: {{ value }}
+    <div v-for="[key, value] of pairs()" :key="key">
+      {{ key }}: <objectItem :value="value" deepness=1 :path="key"/>
     </div>
   </div>
 </template>
 
 <script>
+import objectItem from "./objectitem";
 export default {
-  name: "settingsOBJECT",
+	name: "settingsOBJECT",
+	components: {
+		objectItem,
+	},
   props: ["value"],
   methods: {
     pairs() {
---
+++ ui/src/components/types/objectitem.vue
@@ -0,0 +1,18 @@
+<template>
+	<span>{{value}}</span>
+</template>
+
+<script>
+export default {
+	name: "ObjectItem",
+	props: [
+		'value',
+		'deepness',
+		'path'
+	]
+}
+</script>
+
+<style>
+
+</style>
