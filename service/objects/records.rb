f36429492c5bcb6aa1aaf16bf62ef90fa49f351e
Mik <info@slnt-opp.xyz>
2020-12-02 16:33:06 +0100

    snapshots record class

--- service/objects/records.rb
+++ service/objects/records.rb
@@ -9,9 +9,22 @@ rescue
     puts "Table :records already exists, skipping"
 end
 
+begin
+    $db.create_table :snapshot_records do
+        primary_key :key
+        foreign_key :vm, :vm_pool, null: false
+        Integer     :id, null: false
+        String      :action, size: 3, null: false
+    end
+rescue
+    puts "Table :snapshot_records already exists, skipping"
+end
+
 # History Record Model class
 # @see https://github.com/ione-cloud/ione-sunstone/blob/55a9efd68681829624809b4895a49d750d6e6c34/ione/server/service/objects/records.rb#L1-L10 History Model Defintion
 class Record < Sequel::Model(:records); end
+
+class SnapshotRecord < Sequel::Model(:snapshot_records); end
 
 # States and Notifications records object(linked to VM)
 class OpenNebula::Records
