50d90b6b6d2dc1b34b58a4c7d0f1f6cb6ef1bc1e
Mik <info@slnt-opp.xyz>
2020-12-04 13:52:29 +0100

    comm

--- service/biller.rb
+++ service/biller.rb
@@ -4,10 +4,12 @@ class Biller
         @vm = vm
     end
 
+    # Costs hash
     def costs
         SETTINGS_TABLE.as_hash(:name, :body).select {|key| key.include? 'COST' }
     end
 
+    # Check if this biller should be used in Billing
     def check_biller
         true
     end
