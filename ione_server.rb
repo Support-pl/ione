4cae73a0ba2c6a184b1612fbe0edf60bdcf655ed
Mik <info@slnt-opp.xyz>
2020-12-10 11:18:59 +0100

    Loggin exception into rpc log

--- ione_server.rb
+++ ione_server.rb
@@ -293,6 +293,7 @@ before do
             halt 401, { 'Allow' => "*" }, "False Credentials given"
         end
     rescue => e
+        RPC_LOGGER.debug "Exception #{e.message}"
         RPC_LOGGER.debug "Backtrace #{e.backtrace.inspect}"
         halt 200, { 'Content-Type' => 'application/json', 'Allow' => "*" }, { response: e.message }.to_json
     end
