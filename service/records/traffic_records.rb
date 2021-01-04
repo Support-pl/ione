10d3d93e94c4cb2f6a3e8560bcfa95e3656d56c2
Mik <info@slnt-opp.xyz>
2021-01-04 16:33:31 +0200

    new traffic records synchroniser method

--- service/records/traffic_records.rb
+++ service/records/traffic_records.rb
@@ -23,6 +23,22 @@ class TrafficRecord < Sequel::Model(:traffic_records)
     def mod st
         st[:traffic] = 0
     end
+
+    def conv_keys
+        [:rx, :tx, :rx_last, :tx_last]
+    end
+    def to_i
+        for key in conv_keys do
+            @values[key] = @values[key].to_i
+        end
+        self
+    end
+    def to_s
+        for key in conv_keys do
+            @values[key] = @values[key].to_s
+        end
+        self
+    end
 end
 
 class OpenNebula::TrafficRecords < RecordsSource
@@ -37,37 +53,57 @@ class OpenNebula::TrafficRecords < RecordsSource
     end
 
     def sync vm = nil
-        return 0
-        inserts = 0
-
         if vm.nil? then
             vm = onblock :vm, @id
             vm.info!
         end
 
+        last = TrafficRecord.where(vm: vm.id).order(Sequel.asc(:stime)).last
+        if last.nil? then
+            return 0
+        end
+        last = last.to_i
+
+        def next? ts, last # Filter expression
+            return !last[:etime].nil? && ts <= last[:etime]
+        end
+
         mon_raw = vm.monitoring(['NETTX', 'NETRX'])
         mon = {}
         mon_raw['NETTX'].each do | el |
+            el[0] = el[0].to_i
+            next if next? el.first, last # Filter records which have been counted
             mon[el.first] = {}
             mon[el.first][:tx] = el.last
         end
         mon_raw['NETRX'].each do | el |
+            el[0] = el[0].to_i
+            next if next? el.first, last # Filter records which have been counted
             mon[el.first][:rx] = el.last
-        end
+        end        
 
-        last = TrafficRecord.where(vm: vm.id).order(Sequel.asc(:ts)).last
-        last = { ts: 0 } if last.nil?
+        return 0 if mon.keys.size == 0
 
         for ts, data in mon do
-            ts = ts.to_i
-            if ts > last[:ts] then
-                args = data.merge(vm: vm.id, ts: ts)
-                TrafficRecord.insert **args
-                inserts += 1
+            data[:rx], data[:tx] = data[:rx].to_i, data[:tx].to_i
+            last = last.to_i
+
+            if last[:rx_last] > data[:rx] || last[:tx_last] > data[:tx] then
+                last[:rx] += data[:rx]
+                last[:tx] += data[:tx]
+            else
+                last[:rx] += (data[:rx] - last[:rx_last])
+                last[:tx] += (data[:tx] - last[:tx_last])
             end
+            
+            last[:rx_last], last[:tx_last] = data[:rx], data[:tx]
+            last[:etime] = ts
         end
 
-        inserts
+        last = last.to_s
+        TrafficRecord.where(stime: last[:stime]).update(**last.values)
+
+        mon.keys.size
     end
 
     def find stime, etime
