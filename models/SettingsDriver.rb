00930b2d960545033400c80545ae17ab2cf747c9
Mik <info@slnt-opp.xyz>
2021-01-05 14:20:11 +0200

    better descriptions

--- models/SettingsDriver.rb
+++ models/SettingsDriver.rb
@@ -18,8 +18,8 @@ begin
         ['PUBLIC_NETWORK_DEFAULTS', "{\"NETWORK_ID\":\"network_id\"}", "Default Public Network Pool ID", 1, "object"],
         ['PRIVATE_NETWORK_DEFAULTS', "{\"NETWORK_ID\":\"network_id\"}", "Default Private Network Pool ID", 1, "object"],
         ['CURRENCY_MAIN', "€", "Currency", 0, "str"],
-        ['TRAFFIC_COST', "0.0", "Cost per 1 kByte traffic", 1, "num"],
-        ['SNAPSHOT_COST', "0.0", "Cost 1 Snapshot per sec", 1, "num"]
+        ['TRAFFIC_COST', "0.0", "Cost of 1 kByte VM traffic", 1, "num"],
+        ['SNAPSHOT_COST', "0.0", "Cost of 1 Snapshot per sec", 1, "num"]
     ]
     required.each do | record |
         $db[:settings].insert(name: record[0], body: record[1], description: record[2], access_level: record[3], type: record[4])
