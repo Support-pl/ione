53238acbe928d0dc05078c7932039b3c9f34e243
Mik <info@slnt-opp.xyz>
2021-01-06 17:28:30 +0200

    docs

--- service/records/traffic_records.rb
+++ service/records/traffic_records.rb
@@ -22,13 +22,16 @@ class TrafficRecord < Sequel::Model(:traffic_records)
     end
     alias :ts :sorter
 
+    # Replaces state rx/tx values with current ones
     def mod st
         st.merge! rx: rx.to_i, tx: tx.to_i
     end
 
+    # Keys which do need to be converted from String to Integer
     def conv_keys
         [:rx, :tx, :rx_last, :tx_last]
     end
+    # Convert all values which do need to be converted to Integer
     def to_i
         for key in conv_keys do
             @values[key] = @values[key].to_i
@@ -52,22 +55,24 @@ class OpenNebula::TrafficRecords < RecordsSource
         sync unless nosync
     end
 
+    # Update TrafficRecords with VM monitoring data
     def sync vm = nil
         if vm.nil? then
             vm = onblock :vm, @id
             vm.info!
         end
 
-        last = TrafficRecord.where(vm: vm.id).order(Sequel.asc(:stime)).last
-        if last.nil? then
+        last = TrafficRecord.where(vm: vm.id).order(Sequel.asc(:stime)).last # Get last Trafficrecord
+        if last.nil? then # Give up if nil
             return 0
         end
-        last = last.to_i
+        last = last.to_i # @see TrafficRecord#to_i
 
         def next? ts, last # Filter expression
-            return !last[:etime].nil? && ts <= last[:etime]
+            return !last[:etime].nil? && ts <= last[:etime] # If TrafficRecord is already finished or new record is elder then last update
         end
 
+        # Next block does generate hash structure like { timestamp => {rx, tx} }
         mon_raw = vm.monitoring(['NETTX', 'NETRX'])
         mon = {}
         mon_raw['NETTX'].each do | el |
@@ -82,9 +87,9 @@ class OpenNebula::TrafficRecords < RecordsSource
             mon[el.first][:rx] = el.last
         end        
 
-        return 0 if mon.keys.size == 0
+        return 0 if mon.keys.size == 0 # drop if all of the monitoring records has been skipped
 
-        for ts, data in mon do
+        for ts, data in mon do # Writing new data to active record
             data[:rx], data[:tx] = data[:rx].to_i, data[:tx].to_i
             last = last.to_i
 
@@ -101,21 +106,22 @@ class OpenNebula::TrafficRecords < RecordsSource
         end
 
         last = last.to_s
-        TrafficRecord.where(stime: last[:stime]).update(**last.values)
+        TrafficRecord.where(stime: last[:stime]).update(**last.values) # Write down updated data
 
-        mon.keys.size
+        mon.keys.size # Just for logs
     end
 
+    # Find records between given time and make new active record if last one is bigger than 24h
     def find st, et
         last = TrafficRecord.where(vm: @id).order(Sequel.asc(:stime)).last
-        return EmptyQuery.new if last.nil?
+        return EmptyQuery.new if last.nil? # EmptyQuerySet for Biller
         if last[:etime] - last[:stime] >= 86400 then # If record is elder than 24 hours
             args = last.values.without(:key, :rx, :tx, :stime)
             args.merge! rx: 0, tx: 0, stime: args[:etime] # Setting up new record with zero rx, tx and same rx_last, tx_last
             TrafficRecord.insert(**args.without(:etime))
         end
 
-        @records.exclude(etime: nil).exclude{ etime - stime < 86400 }.where(etime: st..et)
+        @records.exclude(etime: nil).exclude{ etime - stime < 86400 }.where(etime: st..et) # All Records between given time
     end
 
     def init_state stime
