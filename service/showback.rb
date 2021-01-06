e15efa6ea7d86d97e614ba516d3d43ed9d3a9458
Mik <info@slnt-opp.xyz>
2021-01-06 17:13:51 +0200

    docs

--- service/showback.rb
+++ service/showback.rb
@@ -18,20 +18,20 @@ class Timeline
 
     def compile
         records = @sources.inject([]) do | r, source |
-            r.concat source.tl_filter(
-                source.new(@vm.id).find(@stime, @etime).all
+            r.concat source.tl_filter( # Filtering useless events(e.g. unfinished TrafficRecord)
+                source.new(@vm.id).find(@stime, @etime).all # Seeking for all events happend between stime and etime
             )
         end
 
         records.map! do | rec |
-            rec.sortable
+            rec.sortable # Some of Records need to be processed extra to become sortable and usable(for example SnapshotRecords are holding both creation and deletion time, so each SnapshotRecord will be splitted into SnapshotCreateRecord and SnapshotDeleteRecord)
         end
         records.flatten!
     
-        @timeline = records.sort_by { |rec| rec.sorter }
-        @timeline.select! { |rec| rec.sorter.between?(@stime, @etime)}
+        @timeline = records.sort_by { |rec| rec.sorter } # Sorting records by time sorter(.sorter method returns timestamp to be used as sorter)
+        @timeline.select! { |rec| rec.sorter.between?(@stime, @etime)} # Filtering records again
         
-        init
+        init # Generating initial state
         init_rec = InitRecord.new @stime, @state
         @timeline.unshift init_rec
         @timeline << FinalRecord.new(@etime)
@@ -40,12 +40,16 @@ class Timeline
         self
     end
 
+    # Generates initial state
+    # @example VM was turned off a minute before stime, which means init state must be 'off' and versa
+    # @example VM Snapshot was created a minute before stime, which means init state must have 'snaps: 1'
     def init
         @state = @sources.inject({}) do | r, source |
             r.merge source.new(@vm.id).init_state(@stime)
         end
     end
 
+    # InitRecord class to put into the beginning of the Timeline to make it easier to work with deltas
     class InitRecord
         def initialize time, state
             @time, @state = time, state
@@ -57,6 +61,7 @@ class Timeline
             st.merge! @state
         end
     end
+    # Same as InitRecord but for the end of Timeline
     class FinalRecord
         def initialize time
             @time = time
@@ -79,14 +84,18 @@ class Billing
     def initialize vm, stime, etime
         @vm = vm
         @billers = BILLERS.map { | bill | bill.new(@vm) }
-        @billers.select! { |bill| bill.check_biller }
+        # Filtering billers to exclude Billers which won't bill anything. For example there is no Price for Snapshots, so there is no point of billing snapshots for given VM
+        @billers.select! { |bill| bill.check_biller } 
 
+        # Compiling VM Timeline with all of the Events(Records)
         @timeline = Timeline.new vm, stime, etime
         @timeline.compile
     end
 
+    # Generate monstous Hash with debits for each and every event
     def make_bill
         state, @bill = {}, []
+        # Every Record can and will modify VM state at current time, and then Biller will add data to bill basing on current state and time to next event
         @timeline.timeline.each_cons(2) do | curr, con |
             delta = con.ts - curr.ts
             curr.mod state
@@ -100,6 +109,7 @@ class Billing
         @bill
     end
 
+    # Just Adding total to Bill
     def receipt
         @bill.map! do | el |
             el.merge total: el.without(:time).values.sum
