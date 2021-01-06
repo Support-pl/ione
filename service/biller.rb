ba0aa585b6a5ab6ee9fa119be08233b98d692319
Mik <info@slnt-opp.xyz>
2021-01-06 17:16:50 +0200

    docs

--- service/biller.rb
+++ service/biller.rb
@@ -1,3 +1,5 @@
+# Billers parent(actually template) class
+# Needed just to describe what does Biller class should be able to do and provides couple of methods
 class Biller
 
     def initialize vm
@@ -9,7 +11,7 @@ class Biller
         SETTINGS_TABLE.as_hash(:name, :body).select {|key| key.include? 'COST' }
     end
 
-    # Check if this biller should be used in Billing
+    # Check if this biller should be used in Billing !important
     def check_biller
         true
     end
@@ -18,9 +20,12 @@ class Biller
         @vm['//BILLING_PERIOD']
     end
 
+    # Makes bill for given state, delta and record. Modifies :bill hash
+    # @return whatever
     def bill bill:, state:, delta:, record: nil
         0
     end
 end
 
+# Import all available billers from service/billers
 Dir["#{ROOT}/service/billers/*.rb"].each {|file| require file }
