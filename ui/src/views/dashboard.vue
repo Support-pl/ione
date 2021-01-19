056f5527056703398e8a7e70140956bafb17168f
dowellkin <dowellkin@gmail.com>
2021-01-19 14:52:00 +0300

    add timeout

--- ui/src/views/dashboard.vue
+++ ui/src/views/dashboard.vue
@@ -1,26 +1,28 @@
 <template>
-  <a-layout style="min-height: 100%">
-    <a-layout-header class="header"><h1>IONe</h1></a-layout-header>
-    <a-layout>
-      <a-layout-sider>
-        <a-menu
-          mode="inline"
-          v-model="route"
-          :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-menu-item v-for="item in asideBtns" :key="item.name">
-            <router-link :to="{ path: item.route ? item.route : `/dashboard/${item.name}` }">
-              {{item.name | capitalize}}
-            </router-link>
-          </a-menu-item>
+	<div @click='timerUpdate' class='frame' style="min-height: 100%" >
+		<a-layout style="min-height: 100%">
+			<a-layout-header class="header"><h1>IONe</h1></a-layout-header>
+			<a-layout>
+				<a-layout-sider>
+					<a-menu
+						mode="inline"
+						v-model="route"
+						:style="{ height: '100%', borderRight: 0 }"
+					>
+						<a-menu-item v-for="item in asideBtns" :key="item.name">
+							<router-link :to="{ path: item.route ? item.route : `/dashboard/${item.name}` }">
+								{{item.name | capitalize}}
+							</router-link>
+						</a-menu-item>
 
-        </a-menu>
-      </a-layout-sider>
-      <a-layout-content>
-        <router-view />
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+					</a-menu>
+				</a-layout-sider>
+				<a-layout-content>
+					<router-view />
+				</a-layout-content>
+			</a-layout>
+		</a-layout>
+	</div>
 </template>
 
 <script>
@@ -43,17 +45,35 @@ export default {
   data() {
     return {
 			route: [],
-			asideBtns
+			asideBtns,
+			timeoutTimer: -1
     };
   },
   mounted() {
-    this.route = ["settings"];
+		this.route = ["settings"];
+		this.setTimer();
 	},
 	filters: {
 		capitalize(value) {
 			if (!value) return ''
 			value = value.toString()
 			return value.charAt(0).toUpperCase() + value.slice(1)
+		}
+	},
+	methods: {
+		setTimer(){
+			const timeout = 30; // minutes
+			const self = this;
+			this.timeoutTimer = setTimeout(() => {
+				self.$message.warning('Session expired. Return to login page...');
+				setTimeout(() => {
+					location.reload();
+				}, 3000); //3 secs to read message
+			}, timeout * 60000 );
+		},
+		timerUpdate(){
+			clearTimeout(this.timeoutTimer);
+			this.setTimer();
 		}
 	}
 };
