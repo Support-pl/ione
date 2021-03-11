33b2f63d4bef50018f43a9ae682bc6dbce0b6020
Mik <info@slnt-opp.xyz>
2021-03-11 20:30:40 +0100

    DiskBiller with system and backup disks

--- service/billers/disk.rb
+++ service/billers/disk.rb
@@ -3,26 +3,37 @@ class DiskBiller < Biller
   # Checking if Capacity costs are given, otherwise there is no point to calculate it
   def check_biller
     @costs = JSON.parse(costs['DISK_COSTS'])
-    return false if @costs.nil?
-
     res =
       @costs.values.inject(0) do | r, c |
         r += c.to_f
       rescue
         r
       end
-    return false if res <= 0
-
     @cost = @costs[@vm['/VM/USER_TEMPLATE/DRIVE']].to_f
-    @size = @vm.drives.inject(0) { | r, d | r + d['SIZE'].to_i } / 1000.0
-    return @cost > 0
+
+    @costs = JSON.parse(costs['BACKUP_IMAGE_COSTS'])
+    res +=
+      @costs.values.inject(0) do | r, c |
+        r += c.to_f
+      rescue
+        r
+      end
+    @size = @vm.drives.first['SIZE'].to_i / 1000.0
+
+    return res > 0
   rescue
     return false
   end
 
   # @see Biller#bill
-  def bill bill:, _state:, delta:, _record: nil
+  def bill bill:, state:, delta:, record: nil
     bill[:disk] = delta * @cost * @size
+    for rec in state[:system_disk] do
+      bill[:disk] += delta * @cost * rec[:size].to_i / 1000.0
+    end if state[:system_disk]
+    for rec in state[:backup_disk] do
+      bill[:disk] += delta * @costs[rec[:img].to_s].to_f * rec[:size].to_i / 1000.0
+    end if state[:backup_disk]
     bill
   end
 end
