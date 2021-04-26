597ea4fc1b8283428b54758d5e04e15c48bb1357
Mik <info@slnt-opp.xyz>
2021-04-26 21:33:11 +0200

    vlan_leases table

--- models/VLANManager.rb
+++ models/VLANManager.rb
@@ -8,3 +8,16 @@ begin
 rescue
   puts "Table :vlans already exists, skipping"
 end
+
+begin
+  $db.create_table :vlan_leases do
+    primary_key :key
+    foreign_key :vn, :network_pool, null: true, on_delete: :cascade # Network Instantiated with this VLAN ID
+    Integer     :id, null: false               # VLAN ID
+    foreign_key :pool_id, :vlans, null: false  # VLAN Pool ID
+    unique      [:vn, :id]
+  end
+rescue
+  puts "Table :vlan_leases already exists, skipping"
+end
+
