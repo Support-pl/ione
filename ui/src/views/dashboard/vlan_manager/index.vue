e476871681f1fd0bcde8e3fdec16f04b1c98a5f7
Mik <info@slnt-opp.xyz>
2021-04-27 17:27:09 +0200

    vlans index page

--- ui/src/views/dashboard/vlan_manager/index.vue
+++ ui/src/views/dashboard/vlan_manager/index.vue
@@ -1,7 +1,37 @@
 <template>
   <a-row type="flex" justify="space-around" style="margin-top: 1rem">
     <a-col :span="23">
-      {{ pool }}
+      <a-collapse :active-key="Object.keys(pool)">
+        <a-collapse-panel
+          disabled
+          :key="type"
+          :header="type"
+          v-for="(group, type) in pool"
+        >
+          <a-table
+            :columns="vlans_group_table_columns"
+            :data-source="group"
+            rowKey="id"
+            :pagination="false"
+          >
+            <a-row slot="leased" slot-scope="text, record">
+              <a-col :span="4">
+                <h4>
+                  {{ record.leased }} <b>/ {{ record.size }}</b>
+                </h4>
+              </a-col>
+              <a-col :span="20">
+                <a-progress
+                  :percent="(100 * record.leased) / record.size"
+                ></a-progress>
+              </a-col>
+            </a-row>
+          </a-table>
+        </a-collapse-panel>
+      </a-collapse>
+      <a-row>
+        <a-button type="primary" @click="sync">SYNC</a-button>
+      </a-row>
     </a-col>
   </a-row>
 </template>
@@ -13,6 +43,23 @@ export default {
   data() {
     return {
       pool: [],
+      vlans_group_table_columns: [
+        {
+          dataIndex: "id",
+          key: "id",
+          title: "ID",
+        },
+        {
+          dataIndex: "start",
+          key: "start",
+          title: "Start",
+        },
+        {
+          key: "leased",
+          title: "Leased",
+          scopedSlots: { customRender: "leased" },
+        },
+      ],
     };
   },
   mounted() {
@@ -28,9 +75,19 @@ export default {
         url: "/vlan",
         auth: this.credentials,
       }).then((res) => {
-        this.pool = res.data.response;
+        this.pool = res.data.response.reduce(function (r, a) {
+          r[a.type] = r[a.type] || [];
+          r[a.type].push(a);
+          return r;
+        }, {});
       });
     },
   },
 };
 </script>
+
+<style>
+.ant-collapse > .ant-collapse-item > .ant-collapse-header {
+  color: rgba(0, 0, 0, 0.85) !important;
+}
+</style>
