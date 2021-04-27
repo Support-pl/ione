684d4f9c9700285ffd49add805c8b70a28136368
Mik <info@slnt-opp.xyz>
2021-04-27 17:10:00 +0200

    VLAN Manager page base

--- ui/src/views/dashboard/vlan_manager/index.vue
+++ ui/src/views/dashboard/vlan_manager/index.vue
@@ -5,3 +5,32 @@
     </a-col>
   </a-row>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      pool: [],
+    };
+  },
+  mounted() {
+    this.sync();
+  },
+  computed: {
+    ...mapGetters(["credentials"]),
+  },
+  methods: {
+    sync() {
+      this.$axios({
+        method: "get",
+        url: "/vlan",
+        auth: this.credentials,
+      }).then((res) => {
+        this.pool = res.data.response;
+      });
+    },
+  },
+};
+</script>
