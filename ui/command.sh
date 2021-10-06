b1971e9b544f83f25077964ed3ed44c11cd78054
Mik <info@slnt-opp.xyz>
2021-10-06 16:01:43 +0200

    pick IONe UI port via env

--- ui/command.sh
+++ ui/command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "globalThis.VUE_APP_IONE_API_BASE_URL = '"$VUE_APP_IONE_API_BASE_URL"';" > config.js
-thttpd -D -h 0.0.0.0 -p 5000 -d . -u static -l - -M 60
+thttpd -D -h 0.0.0.0 -p "$PORT" -d . -u static -l - -M 60
