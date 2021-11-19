9808aa23464bc336b2729647c99340486df20fe6
Mik <info@slnt-opp.xyz>
2021-11-19 09:47:50 +0100

    always require hook endpoint

--- routes/hooks.rb
+++ routes/hooks.rb
@@ -1,19 +1,16 @@
 # !@group OpenNebula Hooks Endpoints
-if ALPINE then
+require 'open3'
 
-  require 'open3'
-
-  post '/hooks/:hook' do | hook |
-    unless IPAddr.new(request.env['REMOTE_ADDR']).private? then
-      halt 403
-    end
-    hook = "#{ROOT_DIR}/hooks/#{hook}"
-    unless File.exist? hook then
-      halt 400, { 'Content-Type' => 'application/json' }, { error: "Script doesn't exist" }.to_json
-    end
-
-    stdout, stderr, status = Open3.capture3(hook + " " + @request_hash['params'].join(' '))
-    json hook: hook, stdout: stdout, stderr: stderr, status: status.exitstatus
+post '/hooks/:hook' do | hook |
+  unless IPAddr.new(request.env['REMOTE_ADDR']).private? then
+    halt 403
   end
+  hook = "#{ROOT_DIR}/hooks/#{hook}"
+  unless File.exist? hook then
+    halt 400, { 'Content-Type' => 'application/json' }, { error: "Script doesn't exist" }.to_json
+  end
+
+  stdout, stderr, status = Open3.capture3(hook + " " + @request_hash['params'].join(' '))
+  json hook: hook, stdout: stdout, stderr: stderr, status: status.exitstatus
 end
 # !@endgroup
