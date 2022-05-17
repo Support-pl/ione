77afccd48975bf2358fbc43f9e92379315c9b2a9
Mikita Iwanowski <info@slnt-opp.xyz>
2022-05-17 14:32:43 +0200

    thttpd entrypoint to be localhost

--- ui/command.sh
+++ ui/command.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "globalThis.VUE_APP_IONE_API_BASE_URL = '"$VUE_APP_IONE_API_BASE_URL"';" > config.js
-thttpd -D -h 0.0.0.0 -p "$PORT" -d . -u static -l - -M 60
+thttpd -D -h 127.0.0.1 -p "$PORT" -d . -u static -l - -M 60
